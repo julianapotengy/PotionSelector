b80c9b1d6ec5b982ed23bb141ef4d9228d4892bc
Juliana Potengy <jupotengy@gmail.com>
2017-09-24 18:38:02 -0300

    done

---
+++ Assets/Scripts/Editor/PotionSelectorEditor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(PotionSelectorAttribute))]
+public class PotionSelectorEditor : PropertyDrawer
+{
+    int index;
+    string[] potionsNames = new string[PotionDB.Instance.potions.Length];
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        for (int i = 0; i < PotionDB.Instance.potions.Length; i++)
+        {
+            potionsNames[i] = PotionDB.Instance.potions[i].name;
+            if (potionsNames[i] == property.FindPropertyRelative("name").stringValue)
+            {
+                index = i;
+            }
+        }
+
+        Rect popUpRect = new Rect(position.x, position.y, position.width, position.height);
+        index = EditorGUI.Popup(popUpRect, "Potions:", index, potionsNames);
+
+        property.FindPropertyRelative("name").stringValue = PotionDB.Instance.potions[index].name;
+        property.FindPropertyRelative("heal").intValue = PotionDB.Instance.potions[index].heal;
+    }
+}
---
+++ Assets/Scripts/TestBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.AttributeUsage(System.AttributeTargets.Field)]
+public class PotionSelectorAttribute : PropertyAttribute
+{
+    // potion selector attribute script
+}
+
+public class TestBehaviour : MonoBehaviour
+{
+    [PotionSelector]
+    public Potion potion;
+
+    public void Update()
+    {
+        Debug.Log(potion.name + " restores a total of " + potion.heal);
+    }
+}
